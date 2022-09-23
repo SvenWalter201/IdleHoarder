d01d19d6c5e8f9e6a557a657ab12a834a368e38c
Chibiata <robin.pauls@gmx.net>
2022-09-23 23:01:54 +0200

    Menu Zeugs

---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField]
+    string titelScreen = "TitelScreen";
+
+    [SerializeField]
+    string pauseMenuName = "PauseMenu";
+
+    [SerializeField]
+    GameObject PauseMenu = default;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu.SetActive(true);
+        }
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(titelScreen, LoadSceneMode.Single);
+    }
+
+    public void ReturnToGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void EndGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+}
